047d187874506a70bd852a7abe714f8ed824ca02
nikhilx18 <nikhilkumar7718@gmail.com>
2026-06-30 15:06:45 +0530

    Add files via upload

---
+++ includes/database_connect.php
@@ -0,0 +1,8 @@
+<?php
+$db_hostname = "sql207.infinityfree.com";
+$db_username = "if0_42303443";
+$db_password = "Nikhil14474";
+$db_name = "if0_42303443_if0_42303443_pg_life";
+
+$con = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+?>
